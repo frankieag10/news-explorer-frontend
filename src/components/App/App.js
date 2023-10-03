aa8998a912da9b11e1972b3699d1652ac31dfee5
frankieag10 <frankieag10@gmail.com>
2023-10-03 12:13:55 -0700

    App 1.1

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import Header from "../Header/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header className="App-heaader"></Header>
+    </div>
+  );
+}
