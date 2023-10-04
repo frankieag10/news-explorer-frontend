d4bd706dcfeedb859ad83d0a77991b8ec0715215
frankieag10 <frankieag10@gmail.com>
2023-10-04 10:25:32 -0700

    mod.foooter.css 1.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import Header from "../Header/Header.js";
 import Main from "../Main/Main.js";
+import Footer from "../Footer/Footer.js";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
