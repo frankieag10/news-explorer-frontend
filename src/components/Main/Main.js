797b670bf7f84aa77c022378f4e74015e6e10108
frankieag10 <frankieag10@gmail.com>
2023-10-04 11:01:11 -0700

    modify newscard.css 1.1

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Main.css";
 import About from "../About/About";
+import NewsCardList from "../NewsCardList/NewsCardList";
 
 function Main() {
   return (
     <div className="main">
       <About />
+      <NewsCardList />
     </div>
   );
 }
---
+++ src/components/NewsCard/Newscard.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./NewsCard.css";
+
+function NewsCard() {
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+
+  const handleMouseEnter = () => {
+    setTooltipVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setTooltipVisible(false);
+  };
+
+  return (
+    <div className="news-card">
+      <div className="news-card__save-box">
+        {tooltipVisible && <span className="news-card__save-tooltip">Sign in to save articles</span>}
+        <button
+          className="news-card__save-button"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        ></button>
+      </div>
+      <img
+        src={imageCard}
+        alt="News"
+        className="news-card__image"
+      />
+      <div className="news-card__info">
+        <span className="news-card__date">November 4, 2020</span>
+        <h5 className="news-card__title">Everyone Needs a Special 'Sit Spot' in Nature</h5>
+        <p className="news-card__description">
+          Ever since I read Richard Louv's influential book, "Last Child in the Woods," the idea of having a special "sit spot" has stuck with me.
+          This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...
+        </p>
+        <span className="news-card__author">treehugger</span>
+      </div>
+    </div>
+  );
+}
+
+export default NewsCard;
