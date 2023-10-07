9446f480acfaa94e670eb65dd911a16f6e6f18a3
frankieag10 <frankieag10@gmail.com>
2023-10-07 10:38:17 -0700

    newscard/css 1.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Main from "../Main/Main";
 import Footer from "../Footer/Footer.js";
 import "./App.css";
 import SavedNewsHeader from "../SavedNewsHeader/SavedNewsHeader";
+import SavedNews from "../SavedNews/SavedNews";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="App">
       {!isLoggedIn ? <Header /> : <SavedNewsHeader />}
+      {!isLoggedIn ? <Header /> : <SavedNews />}
       <Main />
       <Footer />
     </div>
