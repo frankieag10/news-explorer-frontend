684cef075fa6f0ea98d07d6aac34abb6ae303686
frankieag10 <frankieag10@gmail.com>
2023-10-09 15:44:40 -0700

    main updates

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,12 +2,14 @@ import { useContext } from "react";
 import About from "../About/About";
 import NewsCardList from "../NewsCardList/NewsCardList";
 import IsLoadingContext from "../../contexts/IsLoadingContext";
+import SavedNews from "../SavedNews/SavedNews";
 
 function Main() {
   return (
     <div className="main">
       <About />
       <NewsCardList />
+      <SavedNews />
     </div>
   );
 }
